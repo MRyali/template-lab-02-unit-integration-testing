c2880398654272a88f586202f2817d6abd7c35f5
MRyali <mayurryali29@gmail.com>
2020-01-14 11:11:10 -0800

    Updated to include 3 more test cases

--- test.cpp
+++ test.cpp
@@ -6,6 +6,26 @@ TEST(EchoTest, HelloWorld) {
 	EXPECT_EQ("hello world", echo(3, test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char* test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1, test_val));
+}
+
+TEST(EchoTest, Numbers) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "69";
+	EXPECT_EQ("69", echo(2, test_val)); 
+}
+
+TEST(EchoTest, SpecialChar) {
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "?";
+	EXPECT_EQ("?", echo(2, test_val));
+}
+
+TEST(EchoTest, WordAndChar) {
+	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "!!!";
+	EXPECT_EQ("hello !!!", echo(3, test_val));
+}
+
 int main (int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
