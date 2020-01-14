755426e7b26282e0fa17add820b64f8c0cb36697
MRyali <mayurryali29@gmail.com>
2020-01-14 10:08:10 -0800

    Renamed c-echo.cpp to c-echo.h and converted it to a function rather than a main

--- c-echo.h
+++ c-echo.h
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main(int argv, char** argc) {
-	// Skip the first arg index because its the program
-	for (int i = 1; i < argv; i++) {
-		std::cout << argc[i];
-		// PRint a whitespace after all but the last iteration
-		if (i < argv - 1) {
-			std::cout << " ";
+std::string echo(int length, char** chars) {
+	std::string ret = "";
+	for (int i = 1; i < length; i++) {
+		ret += chars[i];
+		if (i < length - 1) {
+			ret += " ";
 		}
 	}
-	std::cout << std::endl;
+	ret += "\n";
+	return ret;
 }
